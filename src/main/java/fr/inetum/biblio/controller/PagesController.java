fbff433f16ba25c6a773b7ff8161ce4c6a5e4ca2
Valentine <you@example.com>
2022-09-09 09:49:53 +0200

    navigation faite

--- src/main/java/fr/inetum/biblio/controller/PagesController.java
+++ src/main/java/fr/inetum/biblio/controller/PagesController.java
@@ -11,4 +11,24 @@ public class PagesController {
 	public String home() {
 		return "pages/home";
 	}
+	
+	@RequestMapping("/emprunts")
+	public String emprunts() {
+		return "pages/emprunts";
+	}
+	
+	@RequestMapping("/abonnes")
+	public String abonnes() {
+		return "pages/abonnes";
+	}
+	
+	@RequestMapping("/livres-dispo")
+	public String livres() {
+		return "pages/livres-dispo";
+	}
+	
+	@RequestMapping("/recherche")
+	public String recherche() {
+		return "pages/recherche";
+	}
 }
