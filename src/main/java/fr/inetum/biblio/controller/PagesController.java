b9ac16391ea438d984f98f74f2bdad06fce07d7d
Formateur <formateur@m2i.fr>
2022-09-09 09:58:38 +0200

    linked database to app, generated models from db and added method to controller

--- src/main/java/fr/inetum/biblio/controller/PagesController.java
+++ src/main/java/fr/inetum/biblio/controller/PagesController.java
@@ -1,14 +1,29 @@
 package fr.inetum.biblio.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PagesController {
 
 	@RequestMapping("/")
 	public String home() {
+		return "pages/home";
+	}
+	
+	@RequestMapping("/profil")
+	public String profil(HttpServletRequest request) {
+		String nom = request.getParameter("nom");
+		String prenom = request.getParameter("prenom");
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		if(nom != null && prenom != null && email != null && password != null) {
+			return "profil";
+		}
 		return "pages/home";
 	}
 }
